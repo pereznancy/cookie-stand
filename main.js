d85e80a57b171d9fbacb856585b35995123a63be
Nancy Perez <nan.per9@outlook.com>
2018-02-16 22:56:30 -0800

    fixed so many syntax errors..small but annoying

--- main.js
+++ main.js
@@ -1,50 +1,55 @@
 var hoursOpen = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]; //hours for all stores
 
-var getRandom = function getRandomIntInclusive(min, max) { //thank you MDN
+var newRandom = function getRandomIntInclusive(min, max) { //thank you MDN
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //"The maximum is inclusive and the minimum is inclusive" -MDN
 }
 
 //object properties
-var pioneer = {};
-name = "Pioneer Square";
-minCustomer = "17";
-maxCustomer = "88";
-avgCookies = "5.2";
-newCustomer = function (){  //function within a function, fancy.
-  getRandom(this.minCustomer, this.maxCustomer); //still not totally comfortable with this..pun intented
+var pioneer = {
+name: "Pioneer Square",
+minCustomer: "17",
+maxCustomer: "88",
+avgCookies: "5.2",
+newCustomer: function (){  //function within a function, fancy.
+  newRandom(this.minCustomer, this.maxCustomer); //still not totally comfortable with this..pun intented
 }
+};
 
 
-var airport = {};
-name = "Portland Airport";
-minCustomer = "6";
-maxCustoer = "88";
-avgCookies = "5.2";
-newCustomer = function (){
-  getRandom(this.minCustomer, this.maxCustomer);
+var airport = {
+name: "Portland Airport",
+minCustomer: "6",
+maxCustoer: "88",
+avgCookies: "5.2",
+newCustomer: function (){
+  newRandom(this.minCustomer, this.maxCustomer);
   console.log(this.newCustomer);
 }
+};
 
-var washington = {};
-name = "Washington Square";
-minCustomer = "11";
-maxCustoer = "38";
-avgCookies = "1.9";
-newCustomer = function (){
-  getRandom(this.minCustomer, this.maxCustomer);
+var washington = {
+name: "Washington Square",
+minCustomer: "11",
+maxCustoer: "38",
+avgCookies: "1.9",
+newCustomer: function (){
+  newRandom(this.minCustomer, this.maxCustomer);
   console.log(this.newCustomer);
 }
-var pearl = {};
-name = "Pearl District";
-minCustomer = "3";
-maxCustoer = "24";
-avgCookies = "2.6";
-newCustomer = function (){
-  getRandom(this.minCustomer, this.maxCustomer);
+};
+
+var pearl = {
+name: "Pearl District",
+minCustomer: "3",
+maxCustoer: "24",
+avgCookies: "2.6",
+newCustomer: function (){
+  newRandom(this.minCustomer, this.maxCustomer);
   console.log(this.newCustomer);
 }
+};
 
 
 
