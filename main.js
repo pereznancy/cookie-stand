da09923a9db3252d40621b7b444af82266e9cbbf
Nancy Perez <nan.per9@outlook.com>
2018-02-25 14:55:19 -0800

    COMPLETE

--- main.js
+++ main.js
@@ -76,32 +76,3 @@ function makeTable() {
 
 
 makeTable();
-
-
-
-
-
-
-// function isValidEntry(inputField) {
-//   if (inputField.value == "") {
-//     inputField.setAttribute("class", "required");
-//   } else {
-//     inputField.setAttribute("class", "");
-//   }
-// }
-//
-// function validateForm(form) {
-//   var formIsValid = true;
-//   for (var index = 0; index < form.elements.length; index++) {
-//     if (form.elements[index].value == "") {
-//       formIsValid = false;
-//       console.log(form.elements[index].name + " has no value!");
-//     }
-//   }
-//   if (!formIsValid) {
-//     alert("All fields are required. Please review your entries.");
-//     return false;
-//   }
-//   return true;
-//
-// }
