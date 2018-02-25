a21d7c7ff1801d30282262d76502a44e9f445895
Nancy Perez <nan.per9@outlook.com>
2018-02-25 10:10:22 -0800

    table working

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 var hoursOpen = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"] //hours for all stores
 
-
-//function will get cookies ordered based on random customer # & avg cookies bought
 var getCookiesOrdered = function getRandomIntInclusive(minCustomers, maxCustomers, averageCookies) { //thank you MDN
   return Math.floor(((Math.random() * (maxCustomers - minCustomers + 1)) + minCustomers) * averageCookies); //"The maximum is inclusive and the minimum is inclusive" -MDN
 }
-
 
 //Store Constructor
 var Store = function(name, minCustomer, maxCustomer, avgCookies) {
@@ -13,8 +10,8 @@ var Store = function(name, minCustomer, maxCustomer, avgCookies) {
   this.minCustomer = minCustomer;
   this.maxCustomer = maxCustomer;
   this.avgCookies = avgCookies;
-  newAvgCookies = function() {
-    return getCookiesOrdered(this.minCustomer, this.maxCustomer, this.avgCookies);
+  this.newAvgCookies = function() {
+    return getCookiesOrdered(this.minCustomer, this.maxCustomer, this.avgCookies); //"The maximum is inclusive and the minimum is inclusive" -MDN
   }
 }
 
@@ -26,26 +23,18 @@ locations.push(new Store("Washington Square", 11, 38, 1.9));
 locations.push(new Store("Sellwood",20, 48, 3.3));
 locations.push(new Store("Pearl District", 3, 24, 2.6));
 
-//make table
+
 function makeTable() {
   var table = document.getElementById("store-container");
-  table.innerHTML = "<tr><th>Location</th><th>Min Customers</th><th>Max Customers</th><th>Avg Cookies</th>";
-  //for loop to add info to tables
-  for (var index = 0; index < locations.length; index++) {
-    row = document.createElement("tr");
-    cell = document.createElement("td");
-    cell.innerHTML = locations[index].name;
-    row.appendChild(cell);
-    cell = document.createElement("td");
-    cell.innerHTML = locations[index].minCustomer;
-    row.appendChild(cell);
-    cell = document.createElement("td");
-    cell.innerHTML = locations[index].maxCustomer;
-    row.appendChild(cell);
-    cell = document.createElement("td");
-    cell.innerHTML = locations[index].avgCookies;
-    row.appendChild(cell);
-    table.appendChild(row);
+  for (indexStore = 0; indexStore < locations.length; indexStore++) {
+    table.innerHTML += "<tr><th>" + locations[indexStore].name + "</th></th>";
+    var totalCookies = 0
+    for (indexHour = 0; indexHour < hoursOpen.length; indexHour++) {
+      var cookiesPerHour = locations[indexStore].newAvgCookies();
+      table.innerHTML +="<tr><td>" + hoursOpen[indexHour] + "</td><td>" + cookiesPerHour + "</td>"
+      totalCookies += cookiesPerHour;
+    }
+    table.innerHTML += "<tr><td>Total: " + totalCookies + "</tr></td>"
   }
 }
 
